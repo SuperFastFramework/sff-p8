4494322c6d18ad33f2a5ce252520c90ac3a0481c
Iber <iberparodisiri@gmail.com>
2018-02-13 20:52:09 -0300

    Minor fixes

--- sff/collision.lua
+++ sff/collision.lua
@@ -6,8 +6,9 @@ function collides(ent1, ent2)
     local e2b=ent2.bounds
     
     if  ((e1b.xoff1 <= e2b.xoff2 and e1b.xoff2 >= e2b.xoff1)
-    and (e1b.yoff1 <= e2b.yoff2 and e1b.yoff2 >= e2b.yoff1))  
-    return true
+    and (e1b.yoff1 <= e2b.yoff2 and e1b.yoff2 >= e2b.yoff1)) then 
+        return true
+    end
 
     return false
 end
